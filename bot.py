456de8f694087c45e49cfee4fdab47670a7b1f5d
mohmadsharef20-dot <mohmadsharef20@gmail.com>
2025-12-24 21:23:17 +0300

    Initialize bot with handlers and database

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import telebot
+from config import TOKEN
+from database import init_db
+from handlers.user import user_handlers
+from handlers.admin_panel import admin_handlers
 
+bot = telebot.TeleBot(TOKEN)
+
+init_db()
+
+user_handlers(bot)
+admin_handlers(bot)
+
+print("Bot is running...")
+bot.infinity_polling()
